518c243aeb5501ce8340f3813a82818f6ca3e888
spaceyxs <gamzeatli2310@gmail.com>
2024-06-10 20:54:08 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>IMBD</title>
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://kit.fontawesome.com/e5d327bbf3.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <nav>
+        <div class="icon">IMBD</div>
+        <div class="wrapper">
+            <div class="search_box">
+                <div class="dropdown">
+                    <div class="default_option">All</div>
+                    <ul>
+                        <li>All</li>
+                        <li>Titles</li>
+                        <li>Tv Episodes</li>
+                        <li>Celebs</li>
+                        <li>Companies</li>
+                    </ul>
+                </div>
+                <div class="search_field">
+                    <input type="text" class="input" placeholder="Search Here">
+                    <i class="fas fa-search"></i>
+                </div>
+
+            </div>
+        </div>
+
+        <ol>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+
+        </ol>
+    </nav>
+</body>
+</html>
+
